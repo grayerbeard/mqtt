c2eb169114476e211105bd878a8f585f9c85bc4b
Grayerbeard <djgtorrens.gmail.com>
2020-05-11 16:27:34 +0100

    Extensive changes to the test scripts

--- test_publish.py
+++ test_publish.py
@@ -3,9 +3,41 @@
 '''
 Developed By: Dhanish Vijayan
 Company: Elementz Engineers Guild Pvt Ltd
+https://www.elementzonline.com/blog/running-mqtt-broker-in-raspberry-pi
+addapted by David Torrens (https://github.com/grayerbeard/mqtt) based on info from
+http://www.steves-internet-guide.com/into-mqtt-python-client/
+
+Was revised to work with Python3
 '''
+
 import paho.mqtt.client as mqtt
+from time import sleep as time_sleep
+from utility import make_time_text
+from datetime import datetime
+from sys import exit as sys_exit
+
+broker_address = "192.168.0.120" # Change to suite your brokers address
+broker_port = 1883 # Check on server port being used 'sudo netstat -l -t'
+topic_top = "House"
+topic_sub = "test"
+topic_separator = "/"
+count = 0
+
 mqttc = mqtt.Client("python_pub")
-mqttc.connect("192.168.0.120", 1883) # use the ip of your rpi here
-mqttc.publish("Elementz/welcome", "Welcome to Open Source")
-mqttc.loop(2) #timeout = 2s
+mqttc.connect(broker_address, broker_port) # use the ip of your rpi here
+
+
+while True:
+	try:
+		count += 1
+		message =  "msg#: " + str(count) + "  Time Here is " + make_time_text(datetime.now()) 
+		mqttc.publish(topic_top + topic_separator + topic_sub, message)
+		mqttc.loop(2) #timeout = 2s
+		time_sleep(10) # wait a bit before sending again
+	except KeyboardInterrupt:
+		print(".........Ctrl+C pressed... ")
+		message =  "msg#: " + str(count) + "I have been stopped, Time Here is " + make_time_text(datetime.now()) 
+		mqttc.publish(topic_top + topic_separator + topic_sub, message)
+		mqttc.loop(2) #timeout = 2s
+		time_sleep(15) 
+		sys_exit()
--- test_subscribe.py
+++ test_subscribe.py
@@ -2,23 +2,55 @@
 '''
 Developed By: Dhanish Vijayan
 Company: Elementz Engineers Guild Pvt Ltd
+https://www.elementzonline.com/blog/running-mqtt-broker-in-raspberry-pi
+addapted by David Torrens (https://github.com/grayerbeard/mqtt) based on info from
+http://www.steves-internet-guide.com/into-mqtt-python-client/
+
+Was revised to work with Python3
 '''
+
 import paho.mqtt.client as mqtt
+from time import sleep as time_sleep
+from utility import make_time_text
+from datetime import datetime
+
+broker_address = "192.168.0.120" # Change to suite your brokers address
+broker_port = 1883 # Check on server port being used 'sudo netstat -l -t'
+topic_top = "House"
+topic_sub = "test"
+topic_separator = "/"
+count = 0
+house_test_latest.msg = ""
+house_test_latest.count = 0
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(self, client, userdata, rc):
 	print("Connected with result code "+str(rc))
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	self.subscribe("Elementz/welcome")
+	self.subscribe(topic_top + topic_separator + topic_sub)
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	print("Topic: ", msg.topic+'\nMessage: '+str(msg.payload))
+	count += 1
+	if msg.topic = topic_top + topic_separator + topic_sub:
+		house_test_latest.msg = str(msg.payload)
+		house_test_latest.count = count
+	else:
+		print("Topic: ", msg.topic+'\nMessage: '+str(msg.payload))
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.0.120", 1334, 60)
+client.connect(broker_address, broker_port, 60)
+
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
+
 client.loop_forever()
+
+while True:
+	print("Msg# :",house_test_latest.count," Time: ",make_time_text(datetime.now()),"\n","  Latest House/Test Message is : ",house_test_latest,"\n")
+	time_sleep(7) 
